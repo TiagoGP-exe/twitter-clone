6c48fdd44d2e0542615a0d73c290ac9a968eba0f
Tiago Guimarães Pinto <tguimaraespinto@gmail.com>
2023-07-31 09:09:42 -0400

    refactor: ok

--- components/tweet-list.tsx
+++ components/tweet-list.tsx
@@ -6,8 +6,8 @@ import { useEffect, experimental_useOptimistic as useOptimistic } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/navigation";
 import { formattedDate } from "@/lib/utils";
-// import { TweetMoreButton } from "./tweet-more-button";
-// import { useUser } from "@/hooks/useUser";
+import { TweetMoreButton } from "./tweet-more-button";
+import { useUser } from "@/hooks/useUser";
 
 export const dynamic = "force-dynamic";
 
@@ -17,6 +17,8 @@ const maxName2 = (name: string) => {
 };
 
 const TweetList = ({ tweets }: { tweets: TweetWithAuthor[] }) => {
+  const { user } = useUser();
+
   const router = useRouter();
   const [optimisticTweets, addOptimisticTweet] = useOptimistic<
     TweetWithAuthor[],
@@ -80,9 +82,9 @@ const TweetList = ({ tweets }: { tweets: TweetWithAuthor[] }) => {
               {formattedDate(new Date(tweet.created_at))}
             </p>
           </div>
-          {/* {tweet.author.id === user?.id && (
+          {tweet.author.id === user?.id && (
             <TweetMoreButton isOwner={true} tweetId={tweet.id} />
-          )} */}
+          )}
         </div>
         <p className="text-sm flex-1 max-w-[90%] mt-1 break-all">
           {tweet.title}
