a2edc6154d948968566820609152815020cc8232
Tiago Guimarães Pinto <tguimaraespinto@gmail.com>
2023-07-31 09:07:07 -0400

    refactor: adjustment

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import Tweets from "@/components/tweets";
 import TweetList from "@/components/tweet-list";
 
 export const dynamic = "force-dynamic";
